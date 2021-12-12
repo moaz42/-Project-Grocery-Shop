d0b995496c15bf2e53c4444fe1040648af138b15
moaz42 <moazkamal12@gmail.com>
2021-12-12 17:14:08 +0600

    Create dashboard.php

---
+++ Buyer/view/dashboard.php
@@ -0,0 +1,20 @@
+
+<html lang="en">
+<head>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+    </ul>
+        <ul>
+        <li><a href=" viewProfile.php">View Profile</a></li>
+    </ul>
+    <ul>
+        <li><a href="editProfile.php">Edit Profile</a></li>
+    </ul>
+    <ul>
+        <li><a href="search.php">Search</a></li>
+    </ul>
+    <ul> 
+        <li><a href="logout.php">Log Out</a></li>
+    </ul>
+
+</html>

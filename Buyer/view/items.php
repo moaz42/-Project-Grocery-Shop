8a2641bb0d2d0e34f2da9b47f1aebbd9c1d5437f
moaz42 <moazkamal12@gmail.com>
2021-12-12 17:14:22 +0600

    Create items.php

---
+++ Buyer/view/items.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+</head>
+    <title>view Profile</title>
+     <?php include 'navbar.php'?>
+     <link rel="stylesheet" href="../controller/style.css">
+<body>
+<div class="content">
+    <div class="dashboard">
+        <?php 
+            include_once './dashboard.php';
+        ?>
+    </div>
+    <div class="main">
+        <?php include '../model/profile.php'; showallitem();?>
+    </div>
+</div>
+</body>
+
+</html>
